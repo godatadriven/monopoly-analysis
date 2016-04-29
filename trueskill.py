d691bd851b0b3a3c4b114e4d3773782c744bea9f
Vincent D. Warmerdam <vincentwarmerdam@gmail.com>
2016-04-29 15:00:54 +0200

    it now workie workie

--- trueskill.py
+++ trueskill.py
@@ -1,32 +1,69 @@
 import numpy as np
-import pandas as pd
-import json 
 
 size_arr = 100
-d = {} 
+d = {}
 
 def get_player_stats(name):
-    if name not in d.keys(): 
+    """
+    Check if a player exists in the dictionary. If not, add it.
+    Then returns the player belief.
+    """
+    if name not in d.keys():
         d[name] = np.ones(size_arr)/np.sum(np.ones(size_arr))
     return d[name]
 
-def gen_matrix(arr1, arr2):
-    margin = 0
-    mat = np.zeros([len(arr1), len(arr2)]) + 1./size_arr**2/10
-    for i in range(len(arr1)): 
-        for j in range(i - margin, len(arr2)):
-            mat[j,i] = arr1[i] * arr2[j]
-    mat = np.fliplr(mat)/np.sum(mat)
-    return mat 
+def gen_prior(arr1, arr2):
+    """
+    Combines two arrays into a matrix.
+    """
+    return np.matrix(arr1).T * np.matrix(arr2)
 
-def parse_matrix(mat): 
-    new_win = np.sum(mat, 0).reshape(-1)
-    new_lose = np.sum(np.flipud(mat), 1).reshape(-1)
-    return new_win/np.sum(new_win), new_lose/np.sum(new_lose)
+def cut_matrix(mat):
+    """
+    Cuts the matrix according to likelihood update rule.
+    Also applies normalisation
+    """
+    posterior = np.triu(mat)
+    posterior = posterior/np.sum(posterior)
+    return posterior
+
+def gen_marginals(posterior_mat):
+    """
+    From a cut matrix, generate the appropriate marginals back
+    """
+    winner = np.squeeze(np.asarray(np.sum(posterior_mat, 0)))
+    loser = np.squeeze(np.asarray(np.sum(posterior_mat, 1)))
+    return winner, loser
 
 def update(winner, loser):
+    """
+    Given a winner and user, update our state.
+    """
     winner_arr = get_player_stats(winner)
     loser_arr = get_player_stats(loser)
-    new_winner, new_loser = parse_matrix(gen_matrix(winner_arr, loser_arr))
-    d[loser] = new_winner
-    d[winner] = new_loser
+    prior_mat = gen_prior(loser_arr, winner_arr)
+    posterior_mat = cut_matrix(prior_mat)
+    new_winner, new_loser = gen_marginals(posterior_mat)
+    d[loser] = new_loser
+    d[winner] = new_winner
+
+p1 = np.array([1.,2.,3.])
+p2 = np.array([3.,2.,1.])
+p3 = np.array([1.,1.,1.])
+mat1 = gen_prior(p1, p2)
+mat2 = gen_prior(p2, p1)
+mat3 = gen_prior(p3, p3)
+m1, m2 = gen_marginals(mat1)
+m3, m4 = gen_marginals(mat2)
+np1, np2 = gen_marginals(cut_matrix(mat1))
+
+# marginals should still be same shape
+assert all(m1 == m2[::-1])
+# marginals should still be same shape
+assert all(m3 == m4[::-1])
+# matrices should have equal values
+assert np.sum(mat1) == np.sum(mat2)
+# all ones should sum to 9
+assert np.sum(mat3) == 9
+# marginals should still be same shape
+assert all(np1 == np2[::-1])
